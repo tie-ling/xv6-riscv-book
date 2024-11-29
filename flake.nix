156d8f053c566b89bf22580fd9cd29f6e1a9ce6b
Yan Cao <yc@example.com>
2024-11-29 12:17:25 +0100

    flake
    Signed-off-by: Yan Cao <yc@example.com>

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/f45667df53b4a4bb7b0bc0fa4fb83e8c0c51add5";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      formatter.x86_64-linux = pkgs.nixfmt-rfc-style;
+      devShell.x86_64-linux = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          gnumake
+          (pkgs.texliveBasic.withPackages (ps: with ps; [ listings ]);)
+        ];
+      };
+    };
+}
