56c7dd2ff2c21597ce39ff96bcd0dcf1f15c1f92
Yan Cao <yc@example.com>
2024-11-29 12:51:45 +0100

    flake
    Signed-off-by: Yan Cao <yc@example.com>

--- flake.nix
+++ flake.nix
@@ -1,3 +1,4 @@
+
 {
   inputs.nixpkgs.url = "nixpkgs/f45667df53b4a4bb7b0bc0fa4fb83e8c0c51add5";
   # https://pdos.csail.mit.edu/6.1810/
@@ -11,13 +12,19 @@
       devShell.x86_64-linux = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [
           gnumake
-          (pkgs.texliveBasic.withPackages (ps: with ps; [
-            listings xcolor imakeidx
-            xkeyval booktabs etoolbox
-            collection-latexextra
-            metafont
-            collection-fontsrecommended
-          ]))
+          (pkgs.texliveBasic.withPackages (
+            ps: with ps; [
+              listings
+              xcolor
+              imakeidx
+              xkeyval
+              booktabs
+              etoolbox
+              collection-latexextra
+              metafont
+              collection-fontsrecommended
+            ]
+          ))
         ];
       };
     };
